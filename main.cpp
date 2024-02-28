05c56b7558f52476dcf9ce6d27124062a97803dd
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2024-02-28 16:58:50 -0600

    implement geometry and model classes

--- main.cpp
+++ main.cpp
@@ -1,28 +1,82 @@
+#include <cmath>
+#include <vector>
+#include "geometry.hpp"
+#include "model.hpp"
 #include "tgaimage.hpp"
+
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const int width = 800;
+const int height = 800;
+
+Model* model = NULL;
 
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 
-    for (float t = 0.0; t < 1.0; t += 0.01) {
+    bool steep = false;
 
-        int x = x0 * (1.0 - t) + x1 * t;
-        int y = y0 * (1.0 - t) + y1 * t;
+    if(std::abs(x0 - x1) < std::abs(y0 - y1)) {
 
-        image.set(x, y, color);
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+
+        steep = true;
+    }
+
+    if(x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    for(int x = x0; x <= x1; x++) {
+
+        float t = (x - x0)/(float)(x1 - x0);
+        int y = y0*(1.0 - t) + y1*t;
+
+        if(steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
     }
 }
 
 
 int main(int argc, char* argv[]) {
 
-    TGAImage image(100, 100, TGAImage::RGB);
-    line(13, 20, 80, 40, image, white);
+    if(2 == argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+
+    TGAImage image(width, height, TGAImage::RGB);
+
+    for(int i = 0; i < model -> nfaces(); i++) {
+
+        std::vector<int> face = model -> face(i);
+
+        for(int j = 0; j < 3; j++) {
+
+            Vec3f v0 = model -> vert(face[j]);
+            Vec3f v1 = model -> vert(face[(j + 1)%3]);
+            int x0 = (v0.x + 1.0)*width/2.0;
+            int y0 = (v0.y + 1.0)*height/2.0;
+            int x1 = (v1.x + 1.0)*width/2.0;
+            int y1 = (v1.y + 1.0)*height/2.0;
+
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
 
     image.flip_vertically();
     image.write_tga_file("output.tga");
+
+    delete model;
+
+    system("pause");
 
     return 0;
 }
